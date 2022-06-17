58a92b68449797956e0d225556b46ad142f5e7ea
lianze66 <312239109@qq.com>
2022-06-17 10:56:05 +0800

    增加“增、删、改”的方法。

--- src/main/java/org/rsd/controller/SysUserController.java
+++ src/main/java/org/rsd/controller/SysUserController.java
@@ -1,18 +1,14 @@
 package org.rsd.controller;
 
-import com.sun.deploy.net.HttpResponse;
 import org.rsd.bean.SysUser;
 import org.rsd.service.ISysUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @Controller
