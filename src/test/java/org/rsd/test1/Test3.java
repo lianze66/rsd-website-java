411c642f861bc73e3e84695281ad22d59e055efb
lianze66 <312239109@qq.com>
2022-06-20 16:59:05 +0800

    静态代理、JDK动态代理、CGLib动态代理的代码。

--- src/test/java/org/rsd/test1/Test3.java
+++ src/test/java/org/rsd/test1/Test3.java
@@ -1,11 +1,14 @@
 package org.rsd.test1;
 
+import org.junit.jupiter.api.Test;
+
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class Test3 {
 
-    public static void main(String[] args) {
+    @Test
+    public void test1() {
         Apple apple1 = new Apple();
         apple1.setName("红富士");
 
