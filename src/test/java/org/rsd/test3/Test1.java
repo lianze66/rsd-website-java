cace60437433c9248d71f5e50028cfb87ed47151
lianze66 <312239109@qq.com>
2022-06-24 13:18:55 +0800

    实现了树型结构的代码。

--- src/test/java/org/rsd/test3/Test1.java
+++ src/test/java/org/rsd/test3/Test1.java
@@ -1,12 +1,13 @@
 package org.rsd.test3;
 
 import org.junit.jupiter.api.Test;
+import org.rsd.bean.SysFunction;
 import org.rsd.mapper.ISysFunctionMapper;
-import org.rsd.mapper.ISysUserMapper;
-import org.rsd.service.ISysUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import java.util.List;
 
 @SpringJUnitConfig
 @ContextConfiguration("classpath:spring.xml")
@@ -14,5 +15,22 @@ public class Test1 {
 
     @Autowired
     private ISysFunctionMapper sysFunctionMapper;
+
+    void ss(List<SysFunction> rootList) {
+        rootList.forEach(sysFunction -> {
+            List<SysFunction> childList = sysFunctionMapper.getListByParentId(sysFunction.getId());
+            if (childList != null && !childList.isEmpty()) {
+                sysFunction.setChildList(childList);
+                ss(childList);
+            }
+        });
+    }
+
+    @Test
+    void tt() {
+        List<SysFunction> rootList = sysFunctionMapper.getListByParentId(0);
+        ss(rootList);
+        System.out.println(rootList);
+    }
 
 }
